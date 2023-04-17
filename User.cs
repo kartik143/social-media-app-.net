2aa2ab60b3f726ae537928b03cc4056e4908e2f7
kartik143 <38079397+kartik143@users.noreply.github.com>
2023-04-17 15:40:45 +0530

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SocialMediaApp
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        // additional user properties and methods can be added here
+    }
+}
