9a5cb474e369808980cb6d59ffdead9583814b26
kartik143 <38079397+kartik143@users.noreply.github.com>
2023-04-17 15:40:28 +0530

    Create SocialMediaApp.cs

---
+++ SocialMediaApp.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SocialMediaApp
+{
+    class SocialMediaApp
+    {
+        public void Run()
+        {
+            Console.WriteLine("Social Media App is running!");
+            // your application logic here
+        }
+    }
+}
